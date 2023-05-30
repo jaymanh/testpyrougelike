c23572402659b2ceeb22043beeb83c2637940f59
jaymanh <65929600+jaymanh@users.noreply.github.com>
2023-05-30 10:50:00 +1000

    Part  done

--- roguelikepy/input_handlers.py
+++ roguelikepy/input_handlers.py
@@ -50,6 +50,13 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         self.engine = engine
 
     def handle_events(self) -> None:
+        raise NotImplementedError()
+    
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+class MainGameEventHandler(EventHandler):
+    def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
 
@@ -61,8 +68,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             self.engine.handle_enemy_turns()
             self.engine.update_fov() #Update the FOV before the players next action
 
-    def ev_quit(self, event: tcod.event.KeyDown) -> Optional[Action]:
-        raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = NotImplemented
@@ -77,4 +82,26 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         elif key in WAIT_KEYS:
             action = WaitAction(player)
 
+        return action
+
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = NotImplemented
+
+        key = event.sym
+
+        if key == tcod.event.K_ESCAPE:
+            action = EscapeAction(self.engine.player)
+
+        #No valid key was pressed 
         return action
